a38a8e8a5baa2ddcbeff5f55408121daefef2396
Daniel Cadenas <dcadenas@gmail.com>
2008-03-27 21:59:09 +0000

    added missing files!
    git-svn-id: http://depgraph.rubyforge.org/svn/trunk@19 a0089f02-d3f5-4ca6-b7bf-0e686d5b7233

---
+++ lib/nodefinders/gems_node_finder.rb
@@ -0,0 +1,59 @@
+require 'node'
+
+module DepGraph
+  module NodeFinders
+    
+    class GemsNodeFinder
+      def initialize
+        @spec_directories = Gem::SourceIndex.installed_spec_directories
+      end
+      
+      def location=(loc)
+        @spec_directories = loc
+      end
+      
+      def get_nodes
+        require 'rubygems'
+
+        fail 'The gem specification directories were not set' unless @spec_directories and @spec_directories.size > 0
+        
+        nodes = {}
+        @spec_directories.each do |spec_directory|
+          Dir["#{spec_directory}/**/*.gemspec"].each do |gemspec_file_name|
+            add_nodes_from_gemspec(nodes, gemspec_file_name)
+          end
+        end
+        
+        return nodes.values
+      end
+
+      private
+      def add_nodes_from_gemspec(nodes, gemspec_file_name)
+        gem_dependencies = get_gemspec_dependencies(gemspec_file_name)
+        gem_name = get_gemspec_name(gemspec_file_name)
+
+        nodes[gem_name] ||= Node.new(gem_name)
+        gem_dependencies.each do |gem_dependency|
+          nodes[gem_dependency] ||= Node.new(gem_dependency)
+          nodes[gem_name].depends_on(nodes[gem_dependency])
+        end
+      end
+      
+      def get_gemspec_dependencies(gemspec_file_name)
+        gem_dependencies = []
+        content = File.read(gemspec_file_name)
+        content.scan(/add_dependency\(%q<([^>]+)>/) do |matches|
+          matches.each do |match|
+            gem_dependencies << match                
+          end
+        end
+        
+        return gem_dependencies
+      end
+      
+      def get_gemspec_name(gemspec_file_name)
+        File.basename(gemspec_file_name).match(/(.+)-\d+/)[1].to_s        
+      end
+    end
+  end
+end
---
+++ spec/IntegrationTests/gems_node_finder_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+require 'rubygems'
+require 'filetesthelper'
+require 'nodefinders/gems_node_finder'
+
+include FileTestHelper
+include DepGraph::NodeFinders
+
+describe GemsNodeFinder do
+  it "should list all gems and its dependencies" do
+    with_files('fakegem-0.9.1.gemspec' => 's.add_dependency(%q<fakedependencygem>, [">= 1.5.0"])') do
+      gems_node_finder = GemsNodeFinder.new
+      gems_node_finder.location = ['./']
+      nodes = gems_node_finder.get_nodes
+    
+      nodes.should == ['fakedependencygem', 'fakegem']
+      nodes[1].dependencies.size.should == 1
+      nodes[1].dependencies[0].name.should == 'fakedependencygem'
+    end
+  end
+end
