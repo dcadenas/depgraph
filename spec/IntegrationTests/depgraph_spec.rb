a2e7040f06151442db69a294f354ce2f9dc87f09
Daniel Cadenas <dcadenas@gmail.com>
2008-03-30 21:57:35 +0000

    Fixed test bug when paths have spaces
    git-svn-id: http://depgraph.rubyforge.org/svn/trunk@21 a0089f02-d3f5-4ca6-b7bf-0e686d5b7233

--- spec/IntegrationTests/depgraph_spec.rb
+++ spec/IntegrationTests/depgraph_spec.rb
@@ -22,7 +22,7 @@ describe "#{tool_name} (integration tests)" do
     it "should create an image from the #{filter_type} dependency type" do
       test_files = test_data[filter_type]
       with_files(test_files) do
-        system "ruby #{tool_path} -type #{filter_type}"
+        system "ruby \"#{tool_path}\" -type #{filter_type}"
         
         non_empty_file_created(default_graph_file).should be_true
       end
@@ -33,7 +33,7 @@ describe "#{tool_name} (integration tests)" do
   two_files_with_one_dependency_from_file1_to_file2 = {'file1.csproj' => '"file2.csproj"', 'file2.csproj' => ''}
   it 'should not create a file when the "-from" filter does not find matches' do
     with_files(two_files_with_one_dependency_from_file1_to_file2) do
-      system "ruby #{tool_path} -type csproj -from file2"
+      system "ruby \"#{tool_path}\" -type csproj -from file2"
       
       File.exist?(default_graph_file).should be_false
     end
@@ -41,7 +41,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should create a file when the "-from" filter finds matches' do
     with_files(two_files_with_one_dependency_from_file1_to_file2) do
-      system "ruby #{tool_path} -type csproj -from file1"
+      system "ruby \"#{tool_path}\" -type csproj -from file1"
       
       File.exist?(default_graph_file).should be_true
     end
@@ -49,7 +49,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should not create a file when the "-to" filter does not find matches' do
     with_files(two_files_with_one_dependency_from_file1_to_file2) do
-      system "ruby #{tool_path} -type csproj -to file1"
+      system "ruby \"#{tool_path}\" -type csproj -to file1"
       
       File.exist?(default_graph_file).should be_false
     end
@@ -57,7 +57,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should create a file when the "-to" filter finds matches' do
     with_files(two_files_with_one_dependency_from_file1_to_file2) do
-      system "ruby #{tool_path} -type csproj -to file2"
+      system "ruby \"#{tool_path}\" -type csproj -to file2"
       
       File.exist?(default_graph_file).should be_true
     end
@@ -65,7 +65,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should exclude nodes when the "-exc" filter is used' do
     with_files(three_files_with_chained_dependencies_from_file1_to_file2_to_file3) do
-      system "ruby #{tool_path} -type csproj -exc \"file2, ile3\""
+      system "ruby \"#{tool_path}\" -type csproj -exc \"file2, ile3\""
       
       File.exist?(default_graph_file).should be_false #because only file1 exists
     end
@@ -73,7 +73,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should be possible to load the test node finder found in the nodefinders directory' do
     with_files() do
-      system "ruby #{tool_path} -type test"
+      system "ruby \"#{tool_path}\" -type test"
       
       File.exist?(default_graph_file).should be_true
     end
@@ -81,7 +81,7 @@ describe "#{tool_name} (integration tests)" do
   
   it 'should be possible apply a transitive reduction to the output graph' do
     with_files() do
-      system "ruby #{tool_path} -type test -trans"
+      system "ruby \"#{tool_path}\" -type test -trans"
       
       File.exist?(default_graph_file).should be_true
       File.exist?('temp.dot').should be_false
