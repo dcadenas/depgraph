58b69807248b8f7a51660c7211213e70662c974a
Daniel Cadenas <dcadenas@gmail.com>
2012-07-05 04:01:37 -0300

    Bump to 0.11.0

--- Rakefile
+++ Rakefile
@@ -1,4 +1,31 @@
-require 'config/requirements'
-require 'config/hoe'
+require 'rubygems'
+require 'rake'
 
-Dir['tasks/**/*.rake'].each { |rake| load rake }
+spec = Gem::Specification.new do |s|
+  s.name = "depgraph"
+  s.summary = "A tool to create dependency graph images from source code directories"
+  s.description =  "A tool to create dependency graph images from source code directories"
+  s.email = "dcadenas@gmail.com"
+  s.homepage = "http://github.com/dcadenas/depgraph"
+  s.authors = ["Daniel Cadenas"]
+  s.executables = ["depgraph"]
+
+  s.add_development_dependency(%q<rspec>, ["~> 2.8.0"])
+  s.add_dependency(%q<ruby-graphviz>, ["~> 1.0.8"])
+  s.add_dependency(%q<optiflag>, [">= 0.6.5"])
+
+  s.version = File.read("VERSION")
+  s.files = `git ls-files`.split
+end
+
+require 'rake/extensiontask'
+Gem::PackageTask.new(spec) do |pkg|
+end
+
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.rspec_opts = ["-f progress", "-r ./spec/spec_helper.rb"]
+  t.pattern = 'spec/**/*_spec.rb'
+end
+
+task :default => :spec
--- lib/nodefinders/gems_node_finder.rb
+++ lib/nodefinders/gems_node_finder.rb
@@ -2,28 +2,27 @@ require 'node'
 
 module DepGraph
   module NodeFinders
-    
     class GemsNodeFinder
       def initialize
-        @spec_directories = Gem::SourceIndex.installed_spec_directories
+        @spec_directories = Gem::Specification.dirs
       end
-      
+
       def location=(loc)
         @spec_directories = loc
       end
-      
+
       def get_nodes
         require 'rubygems'
 
         fail 'The gem specification directories were not set' unless @spec_directories and @spec_directories.size > 0
-        
+
         nodes = {}
         @spec_directories.each do |spec_directory|
           Dir["#{spec_directory}/**/*.gemspec"].each do |gemspec_file_name|
             add_nodes_from_gemspec(nodes, gemspec_file_name)
           end
         end
-        
+
         return nodes.values.sort
       end
 
@@ -38,21 +37,21 @@ module DepGraph
           nodes[gem_name].depends_on(nodes[gem_dependency])
         end
       end
-      
+
       def get_gemspec_dependencies(gemspec_file_name)
         gem_dependencies = []
         content = File.read(gemspec_file_name)
         content.scan(/add_dependency\(%q<([^>]+)>/) do |matches|
           matches.each do |match|
-            gem_dependencies << match                
+            gem_dependencies << match
           end
         end
-        
+
         return gem_dependencies
       end
-      
+
       def get_gemspec_name(gemspec_file_name)
-        File.basename(gemspec_file_name).match(/(.+)-\d+/)[1].to_s        
+        File.basename(gemspec_file_name).match(/(.+)-\d+/)[1].to_s
       end
     end
   end
